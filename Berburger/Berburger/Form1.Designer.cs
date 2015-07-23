0bc3e76977cabd3f168d3522a2b1548e1373365d
Hoerbl <pascal.hoerburger@student.htldornbirn.at>
2015-07-23 14:55:24 +0200

    Windows Forms draft applied

--- Berburger/Berburger/Form1.Designer.cs
+++ Berburger/Berburger/Form1.Designer.cs
@@ -28,20 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.delete_button = new System.Windows.Forms.Button();
+            this.change_button = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // delete_button
+            // 
+            this.delete_button.Location = new System.Drawing.Point(432, 322);
+            this.delete_button.Name = "delete_button";
+            this.delete_button.Size = new System.Drawing.Size(153, 30);
+            this.delete_button.TabIndex = 0;
+            this.delete_button.Text = "Löschen";
+            this.delete_button.UseVisualStyleBackColor = true;
+            this.delete_button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // change_button
+            // 
+            this.change_button.Location = new System.Drawing.Point(591, 322);
+            this.change_button.Name = "change_button";
+            this.change_button.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.change_button.Size = new System.Drawing.Size(144, 30);
+            this.change_button.TabIndex = 1;
+            this.change_button.Text = "Ändern";
+            this.change_button.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(723, 24);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 52);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(723, 264);
+            this.dataGridView1.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(310, 278);
+            this.ClientSize = new System.Drawing.Size(747, 364);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.change_button);
+            this.Controls.Add(this.delete_button);
             this.Name = "Form1";
             this.Text = "Berburger";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button delete_button;
+        private System.Windows.Forms.Button change_button;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+
     }
 }
 
