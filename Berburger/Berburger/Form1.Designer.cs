5355572a794b777cca08a6123e2dc2f195060ca7
David Bernhard <david.bernhard@student.htldornbirn.at>
2015-07-26 12:07:07 +0200

    Form1 is now responsive and changed indents to tabs

--- Berburger/Berburger/Form1.Designer.cs
+++ Berburger/Berburger/Form1.Designer.cs
@@ -28,63 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.delete_button = new System.Windows.Forms.Button();
-            this.change_button = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // delete_button
-            // 
-            this.delete_button.Location = new System.Drawing.Point(432, 322);
-            this.delete_button.Name = "delete_button";
-            this.delete_button.Size = new System.Drawing.Size(153, 30);
-            this.delete_button.TabIndex = 0;
-            this.delete_button.Text = "Löschen";
-            this.delete_button.UseVisualStyleBackColor = true;
-            this.delete_button.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // change_button
-            // 
-            this.change_button.Location = new System.Drawing.Point(591, 322);
-            this.change_button.Name = "change_button";
-            this.change_button.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.change_button.Size = new System.Drawing.Size(144, 30);
-            this.change_button.TabIndex = 1;
-            this.change_button.Text = "Ändern";
-            this.change_button.UseVisualStyleBackColor = true;
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(12, 12);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(723, 24);
-            this.comboBox1.TabIndex = 2;
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 52);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(723, 264);
-            this.dataGridView1.TabIndex = 3;
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(747, 364);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.change_button);
-            this.Controls.Add(this.delete_button);
-            this.Name = "Form1";
-            this.Text = "Berburger";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            this.ResumeLayout(false);
+			this.delete_button = new System.Windows.Forms.Button();
+			this.change_button = new System.Windows.Forms.Button();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// delete_button
+			// 
+			this.delete_button.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.delete_button.Location = new System.Drawing.Point(324, 262);
+			this.delete_button.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.delete_button.Name = "delete_button";
+			this.delete_button.Size = new System.Drawing.Size(115, 24);
+			this.delete_button.TabIndex = 0;
+			this.delete_button.Text = "Löschen";
+			this.delete_button.UseVisualStyleBackColor = true;
+			this.delete_button.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// change_button
+			// 
+			this.change_button.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.change_button.Location = new System.Drawing.Point(443, 262);
+			this.change_button.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.change_button.Name = "change_button";
+			this.change_button.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+			this.change_button.Size = new System.Drawing.Size(108, 24);
+			this.change_button.TabIndex = 1;
+			this.change_button.Text = "Ändern";
+			this.change_button.UseVisualStyleBackColor = true;
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(9, 10);
+			this.comboBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(173, 21);
+			this.comboBox1.TabIndex = 2;
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Location = new System.Drawing.Point(9, 42);
+			this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.RowTemplate.Height = 24;
+			this.dataGridView1.Size = new System.Drawing.Size(542, 214);
+			this.dataGridView1.TabIndex = 3;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(560, 296);
+			this.Controls.Add(this.dataGridView1);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.change_button);
+			this.Controls.Add(this.delete_button);
+			this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.Name = "Form1";
+			this.Text = "Berburger";
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.ResumeLayout(false);
 
         }
 
