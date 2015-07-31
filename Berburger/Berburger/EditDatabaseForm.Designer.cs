ec8b5a3ff3b7b0e0cbb8c3f5ed0c999b0e94a7fe
Babble- <david.bernhard@student.htldornbirn.at>
2015-07-31 23:40:04 +0200

    EditDatabaseForm: Autosize all headers and make dataGridView readonly

--- Berburger/Berburger/EditDatabaseForm.Designer.cs
+++ Berburger/Berburger/EditDatabaseForm.Designer.cs
@@ -77,9 +77,11 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
 			this.dataGridView1.Location = new System.Drawing.Point(11, 44);
 			this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
 			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
 			this.dataGridView1.RowTemplate.Height = 24;
 			this.dataGridView1.Size = new System.Drawing.Size(902, 401);
 			this.dataGridView1.TabIndex = 3;
